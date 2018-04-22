e3d1f75cefce343c43a234c6e82867b76f451133
Justin Walsh <contact.me@thejustinwalsh.com>
2018-04-21 20:21:59 -0700

    Title / Game Over Polish
    Make my name fancy, and add a space to GameOver

--- src/scene/GameOver.hx
+++ src/scene/GameOver.hx
@@ -7,7 +7,7 @@ class GameOver extends Scene
         hxd.Stage.getInstance().addEventTarget(onEvent);
 
         var title = new h2d.Text(hxd.Res.fonts.blueman10.toFont(), this);
-        title.text = "GameOver";
+        title.text = "Game Over";
         title.textAlign = Center;
         title.x = hxd.Stage.getInstance().width / 2;
         title.y = hxd.Stage.getInstance().height / 2 - 24;
--- src/scene/Title.hx
+++ src/scene/Title.hx
@@ -2,15 +2,28 @@ package scene;
 
 class Title extends Scene
 {
+    var title:h2d.Text;
+    var author:h2d.Text;
+
+    var cAlpha:Float = 0.0;
+    var cStart:Int = 0x29adff;
+    var cEnd:Int = 0xff004d;
+
     override public function init()
     {
         hxd.Stage.getInstance().addEventTarget(onEvent);
 
-        var title = new h2d.Text(hxd.Res.fonts.blueman10.toFont(), this);
+        title = new h2d.Text(hxd.Res.fonts.blueman10.toFont(), this);
         title.text = "Ludum Dare 41";
         title.textAlign = Center;
         title.x = hxd.Stage.getInstance().width / 2;
         title.y = hxd.Stage.getInstance().height / 2 - 24;
+
+        author = new h2d.Text(hxd.Res.fonts.tinyunicode12.toFont(), this);
+        author.text = "justin walsh";
+        author.textAlign = Center;
+        author.x = hxd.Stage.getInstance().width / 2;
+        author.y = title.y + 28;
     }
 
     function onEvent(event:hxd.Event)
@@ -19,6 +32,23 @@ class Title extends Scene
         {
             complete = true;
         }
+    }
+
+    override public function update(dt:Float)
+    {
+        cAlpha += 0.001 * dt;
+        if (cAlpha >= 1.0) {
+            cAlpha = 1.0 - cAlpha;
+            var end = cStart;
+            cStart = cEnd;
+            cEnd = end;
+        }
+
+        var start = h3d.Vector.fromColor(cStart, 1.0);
+        var end = h3d.Vector.fromColor(cEnd, 1.0);
+        var color = new h3d.Vector(1.0, 1.0, 1.0, 1.0);
+        color.lerp(start, end, cAlpha);
+        author.textColor = color.toColor();
     }
 
     override public function shutdown()
