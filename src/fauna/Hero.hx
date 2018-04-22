aaba449e25e1f4234e949c0e4304497ad5352aa4
Justin Walsh <contact.me@thejustinwalsh.com>
2018-04-22 10:17:56 -0700

    Hero SFX
    Adding audio when the hero collides with a wall

--- src/fauna/Hero.hx
+++ src/fauna/Hero.hx
@@ -20,10 +20,15 @@ class Hero extends Fauna
 
     var travelDir:HeroMoveDirection = Stop;
 
+    var hitEffects:Array<hxd.res.Sound> = [];
+
     override public function init()
     {
         // Create placeholder hero art
-        new h2d.Bitmap(h2d.Tile.fromColor(0xff004d, TILE_SIZE-1, TILE_SIZE-1), this);
+        new h2d.Bitmap(h2d.Tile.fromColor(0xff004d, TILE_SIZE, TILE_SIZE), this);
+
+        // Load Audio
+        hitEffects = [hxd.Res.sfx.Hit_Hurt3, hxd.Res.sfx.Hit_Hurt4, hxd.Res.sfx.Hit_Hurt5, hxd.Res.sfx.Hit_Hurt6, hxd.Res.sfx.Hit_Hurt7];
     }
 
     override public function update(dt:Float)
@@ -31,48 +36,52 @@ class Hero extends Fauna
         switch (travelDir) {
             case Up: {
                 var targetX = x;
-                var targetY = Math.floor(y - MOVESPEED * dt);
+                var targetY = Math.floor(y - MOVESPEED);
                 if (check(targetX, targetY, 0, collideWith)) {
                     this.y = targetY;
                 }
                 else {
                     this.y = (Math.floor(targetY / TILE_SIZE) * TILE_SIZE) + TILE_SIZE;
+                    hitEffects[Math.floor(Math.random() * hitEffects.length)].play();
                     travelDir = Stop;
                 }
             };
 
             case Left: {
-                var targetX = Math.floor(x - MOVESPEED * dt);
+                var targetX = Math.floor(x - MOVESPEED);
                 var targetY = y;
                 if (check(targetX, targetY, 0, collideWith)) {
                     this.x = targetX; 
                 }
                 else {
                     this.x = (Math.floor(targetX / TILE_SIZE) * TILE_SIZE) + TILE_SIZE;
+                    hitEffects[Math.floor(Math.random() * hitEffects.length)].play();
                     travelDir = Stop;
                 }
             };
 
             case Down: {
                 var targetX = x;
-                var targetY = Math.floor(y + MOVESPEED * dt);
+                var targetY = Math.floor(y + MOVESPEED);
                 if (check(targetX, targetY + TILE_SIZE, 0, collideWith)) {
                     this.y = targetY; 
                 }
                 else {
                     this.y = (Math.floor((targetY + TILE_SIZE) / TILE_SIZE) * TILE_SIZE) - TILE_SIZE;
+                    hitEffects[Math.floor(Math.random() * hitEffects.length)].play();
                     travelDir = Stop;
                 }
             };
 
             case Right: {
-                var targetX = Math.floor(x + MOVESPEED * dt);
+                var targetX = Math.floor(x + MOVESPEED);
                 var targetY = y;
                 if (check(targetX + TILE_SIZE, targetY, 0, collideWith)) {
                     this.x = targetX;
                 }
                 else {
                     this.x = (Math.floor((targetX + TILE_SIZE) / TILE_SIZE) * TILE_SIZE) - TILE_SIZE;
+                    hitEffects[Math.floor(Math.random() * hitEffects.length)].play();
                     travelDir = Stop;
                 }
             };
